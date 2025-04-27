d8e7420250003d9dfd6339996240fe4367b567e6
Igor Rogov <igor.rogov.35@gmail.com>
2025-04-27 17:28:30 +1000

    initial release with very simple tracing

---
+++ src/main.rs
@@ -0,0 +1,210 @@
+use clap::Parser;
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::DataLinkReceiver;
+use pnet::packet::Packet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::icmp::echo_request::{EchoRequestPacket, MutableEchoRequestPacket};
+use pnet::packet::icmp::{IcmpCode, IcmpPacket, IcmpTypes, checksum};
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::transport::TransportChannelType::Layer4;
+use pnet::transport::TransportProtocol::Ipv4;
+use pnet::transport::transport_channel;
+use pnet::util::MacAddr;
+use std::net::{IpAddr, Ipv4Addr};
+use std::thread::JoinHandle;
+use std::time::Duration;
+use std::{process, thread};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// List all interfaces and exit
+    #[arg(short, long)]
+    list: bool,
+
+    /// IP address to traceroute
+    #[arg(required_unless_present = "list")]
+    address: Option<String>,
+
+    /// Index of the network interface to use (use --list to list of interfaces)
+    #[arg(short, long)]
+    #[arg(required_unless_present = "list")]
+    interface: Option<u32>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.list {
+        list_interfaces_and_exit();
+    }
+
+    let target_address_string = args.address.expect("Address must be specified");
+    let target_address = target_address_string
+        .parse()
+        .expect(format!("Invalid address: {}", target_address_string).as_str());
+
+    let iface_index = args.interface.expect("Interface must be specified");
+    let iface = datalink::interfaces()
+        .into_iter()
+        .find(|i| i.index == iface_index)
+        .expect(format!("Could not find interface by index: {}", iface_index).as_str());
+
+    println!(
+        "Using interface: {} ({}, index: {})",
+        iface.description,
+        iface
+            .ips
+            .iter()
+            .map(|ip| ip.to_string())
+            .collect::<Vec<String>>()
+            .join(", "),
+        iface.index
+    );
+
+    let (_, rx) = match datalink::channel(&iface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("unhandled channel type"),
+        Err(e) => panic!("unable to create channel: {}", e),
+    };
+
+    let thread = listen_icmp(iface.mac.unwrap(), target_address, rx);
+
+    let (mut sender, _) = transport_channel(1024, Layer4(Ipv4(IpNextHeaderProtocols::Icmp)))
+        .expect("transport_channel");
+
+    // TODO: wait until the first packet is sent
+    thread::sleep(Duration::from_secs(1));
+
+    let mut attempt = 0;
+    loop {
+        attempt += 1;
+        let mut icmp_buf: Vec<u8> = vec![0; EchoRequestPacket::minimum_packet_size()];
+        build_echo_request(&mut icmp_buf, attempt);
+        sender.set_ttl(attempt).expect("failed to set ttl");
+        sender
+            .send_to(
+                IcmpPacket::new(&icmp_buf[..]).unwrap(),
+                IpAddr::V4(target_address),
+            )
+            .unwrap();
+
+        thread::sleep(Duration::from_millis(500));
+
+        if attempt >= 20 {
+            break;
+        }
+    }
+
+    thread.join().expect("failed to join thread");
+}
+
+fn list_interfaces_and_exit() {
+    let mut interfaces = datalink::interfaces();
+    interfaces.sort_by_key(|k| k.index);
+    interfaces.iter().for_each(|i| {
+        let ips = i
+            .ips
+            .iter()
+            .filter(|ip| ip.is_ipv4())
+            .map(|ip| ip.ip())
+            .filter(|ip| !ip.is_unspecified())
+            .map(|ip| ip.to_string())
+            .collect::<Vec<String>>();
+
+        if !ips.is_empty() {
+            println!(
+                "Index: {:2}, IP: {}, Description: {}",
+                i.index,
+                ips.join(", "),
+                i.description
+            );
+        }
+    });
+
+    process::exit(0);
+}
+
+fn build_echo_request(buf: &mut [u8], attempt: u8) {
+    let mut echo_packet = MutableEchoRequestPacket::new(buf).unwrap();
+
+    echo_packet.set_sequence_number(attempt as u16);
+    echo_packet.set_identifier(0x123);
+    echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    echo_packet.set_icmp_code(IcmpCode::new(0));
+
+    let echo_checksum = checksum(&IcmpPacket::new(echo_packet.packet()).unwrap());
+    echo_packet.set_checksum(echo_checksum);
+}
+
+fn listen_icmp(
+    iface_mac: MacAddr,
+    target_ip: Ipv4Addr,
+    mut rx: Box<dyn DataLinkReceiver>,
+) -> JoinHandle<()> {
+    thread::spawn(move || {
+        println!("Listening for ICMP packets...");
+        loop {
+            match rx.next() {
+                Ok(packet_data) => {
+                    if handle_packet(iface_mac, target_ip, packet_data) {
+                        return;
+                    }
+                }
+                Err(e) => panic!("packetdump: unable to receive packet: {}", e),
+            }
+        }
+    })
+}
+
+fn handle_packet(iface_mac: MacAddr, target_ip: Ipv4Addr, packet_data: &[u8]) -> bool {
+    let eth_packet = EthernetPacket::new(packet_data).unwrap();
+
+    // TODO: implement better filtering
+    if iface_mac == eth_packet.get_source() {
+        // outgoing packet
+        return false;
+    }
+
+    match eth_packet.get_ethertype() {
+        EtherTypes::Ipv4 => {
+            let ipv4 = Ipv4Packet::new(eth_packet.payload()).unwrap();
+            if ipv4.get_next_level_protocol() == IpNextHeaderProtocols::Icmp {
+                let icmp_packet = IcmpPacket::new(ipv4.payload()).expect("Icmp packet");
+                println!(
+                    "Received from {:16} - code: {:2}, type: {:2}, seq: {:3}",
+                    ipv4.get_source(),
+                    icmp_packet.get_icmp_code().0,
+                    icmp_packet.get_icmp_type().0,
+                    get_seq_number(&icmp_packet)
+                );
+
+                // TODO: implement proper exit
+                if ipv4.get_source() == target_ip {
+                    // final IP reached
+                    return true;
+                }
+            }
+        }
+        _ => {
+            // ignore
+        }
+    }
+    false
+}
+
+fn get_seq_number(icmp_packet: &IcmpPacket) -> u32 {
+    let icmp_payload = icmp_packet.payload();
+    if icmp_payload.len() < Ipv4Packet::minimum_packet_size() + IcmpPacket::minimum_packet_size() {
+        // no payload
+        return 0;
+    }
+
+    let original_icmp = IcmpPacket::new(&icmp_payload[Ipv4Packet::minimum_packet_size()..])
+        .expect("Original ICMP Packet");
+    let original_echo_request =
+        EchoRequestPacket::new(original_icmp.payload()).expect("Original Echo Request");
+    original_echo_request.get_sequence_number() as u32
+}
